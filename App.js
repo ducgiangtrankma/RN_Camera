889e2442623671581d8411214dea5a63c640cc14
ducgiangtrankma <tranducgiangact@gmail.com>
2020-11-17 16:40:28 +0700

    add-rn-camera

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+export default function App(props) {
+  return (
+    <SafeAreaView>
+      <Text>Test</Text>
+    </SafeAreaView>
+  );
+}
